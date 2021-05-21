4aa91f5e3d7df8f9ade0902013a358c8352a1c63
Joshua R Licona <77369494+joshua-licona02@users.noreply.github.com>
2021-05-20 22:55:49 -0400

    Add files via upload

---
+++ oddNumChar.java
@@ -0,0 +1,26 @@
+package csi.vmi.edu;
+
+import java.util.Scanner;
+
+/*
+ * Author: Joshua Licona
+ * Purpose: print odd number characters
+ */
+public class oddNumChar {
+
+	public static void main(String[]args) {
+		Scanner input = new Scanner(System.in);
+
+		
+		System.out.print("Enter a string: ");
+		String string = input.nextLine();
+
+		String oddPositions = " ";	
+	
+		for (int i = 0; i < string.length(); i+=2) {
+			oddPositions += string.charAt(i);
+		}
+
+		System.out.println(oddPositions);
+	}
+}
